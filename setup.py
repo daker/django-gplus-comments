58092dca0ef91d9e58bacf14d88cdb1010da9051
Adnane Belmadiaf <adnane002@gmail.com>
2013-04-24 20:49:10 +0000

    Fixed packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from gplus_comments import __version__
 
 setup(
     name='django-gplus-comments',
@@ -8,12 +9,21 @@ setup(
     description='Integrate Google+ comments into your Django website',
     author='Adnane Belmadiaf',
     author_email='adnane002@gmail.com',
-    license='New BSD License',
+    url='https://github.com/daker/django-gplus-comments',
+    license=__version__,
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Topic :: Communications',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ],
     include_package_data=True,
     packages=find_packages(),
